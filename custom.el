a808299e282209cb630f758fd9fa87b93bccaad4
Matthew Madurski <mmadurski@managedbyq.com>
2015-07-23 15:21:03 -0400

    jsx support in web-mode

--- custom.el
+++ custom.el
@@ -105,3 +105,25 @@
       (setq tab-width 4)
       (setq standard-indent 4)
       (setq indent-tabs-mode nil)))
+
+(add-to-list 'load-path "~/.emacs.d/vendor/jade-mode")
+(require 'sws-mode)
+(require 'jade-mode)
+(add-to-list 'auto-mode-alist '("\\.styl\\'" . sws-mode))
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
+(setq js-indent-level 2)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-code-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
